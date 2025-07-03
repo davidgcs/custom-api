c4f1bfed2077bd4da515d8bc6284212a4a3180e5
David Gomez <dgomezcs94@gmail.com>
2025-07-03 23:34:25 +0200

    added express, json server and return api for users and a count

--- server.js
+++ server.js
@@ -1,11 +1,39 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-app.get("/saludo", (req, res) => {
-  res.json({ mensaje: "Hola desde la API local" });
+app.get("/users", (req, res) => {
+  fs.readFile("./users.json", "utf8", (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: "Error al leer db.json" });
+    }
+
+    try {
+      const db = JSON.parse(data);
+      res.json(db.users || []);
+    } catch (parseError) {
+      res.status(500).json({ error: "Error al procesar los datos" });
+    }
+  });
+});
+
+app.get("/users/count", (req, res) => {
+  fs.readFile("./users.json", "utf8", (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: "Error al leer db.json" });
+    }
+
+    try {
+      const db = JSON.parse(data);
+      const total = db.users?.length || 0;
+      res.send(total.toString() || "0");
+    } catch (parseError) {
+      res.status(500).json({ error: "Error al procesar los datos" });
+    }
+  });
 });
 
 app.listen(PORT, () => {
